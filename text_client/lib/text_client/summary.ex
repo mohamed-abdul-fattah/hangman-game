948d9c02bcc69a2a576c443350e84578b800c16c
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2020-10-19 23:54:39 +0200

    Displays letters used so far

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -1,11 +1,12 @@
 defmodule TextClient.Summary do
   alias TextClient.State
 
-  def display(game = %State{ tally: tally }) do
+  def display(game = %State{ game_service: gs, tally: tally }) do
     IO.puts [
       "\n",
       "Word fo far:  #{Enum.join(tally.letters, " ")}\n",
-      "Guesses left: #{tally.turns_left}"
+      "Guesses left: #{tally.turns_left}\n",
+      "Letters used so far: #{Enum.join(gs.used, ", ")}"
     ]
     game
   end
