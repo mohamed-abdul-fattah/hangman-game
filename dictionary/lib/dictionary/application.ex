eab95399962a461ac0d4e89fc169c490f1e75264
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2020-10-22 14:29:22 +0200

    Supervise the Agent process

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -1,5 +1,13 @@
 defmodule Dictionary.Application do
   def start(_type, _args) do
-    Dictionary.WordList.start_link
+    import Supervisor.Spec
+    children = [worker(Dictionary.WordList, [])]
+    options  = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one,
+      max_restarts: 8,
+      max_seconds: 5,
+    ]
+    Supervisor.start_link(children, options)
   end
 end
