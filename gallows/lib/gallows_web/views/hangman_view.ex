5e6abe017fe832ba24185385940c8c78c27ca8f8
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2020-11-02 11:57:09 +0200

    Adding nice alert box explaning the current game state

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,36 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  @states %{
+    won:           { :success, "You won!" },
+    lost:          { :danger, "You lost!" },
+    good_guess:    { :success, "Good guess!" },
+    bad_guess:     { :warning, "Bad guess!" },
+    already_used:  { :info, "You already guessed that!" }
+  }
+
   def word_so_far(tally = %{ letters: _list }) do
     tally.letters |> Enum.join(" ")
+  end
+
+  def game_state(state) do
+    @states[state]
+    |> alert()
+  end
+
+  def new_game_btn(conn) do
+    button("Create a new game", to: Routes.hangman_path(conn, :store))
+  end
+
+  def game_over?(%{game_state: state}) do
+    state in [:won, :lost]
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, msg}) do
+    """
+      <div class="alert alert-#{class}">#{msg}</div>
+    """
+    |> raw()
   end
 end
